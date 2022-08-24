1d3d9a459fd15efa53f1657ca932352f47aa5651
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-08-24 13:49:44 +0200

    responsive footer

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,6 +3,10 @@ import Banner from '../../components/Banner';
 import bannerAbout from '../../assets/bannerAbout.png';
 import './about.css';
 
+/** Création de la page About
+ * @return {JSX.Element}
+ */
+
 const About = () => {
   return (
     <main>
--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+/** Création page erreur 404
+ * @return {JSX.Element}
+ */
+
 const Error404 = () => {
-  return <div>Erreur 404 !</div>;
+  return <main>Erreur 404 !</main>;
 };
 
 export default Error404;
