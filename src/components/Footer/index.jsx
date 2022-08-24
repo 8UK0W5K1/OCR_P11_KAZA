3bb39dbaf0c7c49f4b58d9104708250fd05c2a3f
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-08-24 12:46:07 +0200

    footer ok w/ styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import footerLogo from '../../assets/footerLogo.png';
+import './footer.css';
 
 const Footer = () => {
-  return <div>Footer!</div>;
+  return (
+    <footer>
+      <img className='footer__logo' src={footerLogo} alt=' Logo Kasa' />
+      <p className='footer__copyright'>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
 };
 
 export default Footer;
