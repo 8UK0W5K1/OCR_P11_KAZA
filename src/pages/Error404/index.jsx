29cabc1467fa45e719aedf488b1ce4b5aeebdf28
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-08-24 13:57:58 +0200

    page404 w/ its styles & responsive finished

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './error404.css';
 
 /** Création page erreur 404
  * @return {JSX.Element}
  */
 
 const Error404 = () => {
-  return <main>Erreur 404 !</main>;
+  return (
+    <main>
+      <section className='error__section'>
+        <h1 className='error__title'>404</h1>
+        <p className='error__content'>
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link to='/'>
+          <p className='error__link'>Retourner sur la page d’accueil</p>
+        </Link>
+      </section>
+    </main>
+  );
 };
 
 export default Error404;
