2a36e741e5e5a4ccc1b7b77282416c9a168f1f07
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-08-26 14:38:40 +0200

    problem w/ fetch on json file

---
+++ src/Gallery/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/** Création galerie page d'accueil
+ * @param {array}  - Array logements
+ * @return {JSX.Element}
+ */
+
+const Gallery = () => {
+  return (
+    <section className='gallery'>
+      ici la gallerie avec map sur datas
+      {/* {housingsData.map(({ title }) => (
+        <ul>
+          <li>li du ul {title}</li>
+        </ul>
+      ))} */}
+    </section>
+  );
+};
+
+export default Gallery;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Banner from '../../components/Banner';
 import bannerHome from '../../assets/bannerHome.png';
+import Gallery from '../../Gallery';
+import { housingsData } from '../../data/housingsData.json';
+console.log({ housingsData });
+
 /**
  * Création de la page d'accueil
  *
@@ -8,13 +13,29 @@ import bannerHome from '../../assets/bannerHome.png';
  * @return {JSX.Element}
  */
 
-//
+// initialisation des données json
 
-const Home = ({ housingsData }) => {
+const Home = () => {
+  const [Data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`../../data/housingsData.json`);
+        const housingsData = await response.json();
+        console.log(housingsData);
+        setData(housingsData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <main>
       <Banner image={bannerHome} type='Home' />
-      <h1>Homepage!</h1>
+      <Gallery />
     </main>
   );
 };
