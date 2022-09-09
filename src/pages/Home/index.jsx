020199a1eb8cdc2ca9e1710c944fd9b83e3515ab
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-09-09 14:54:07 +0200

    problem w/ fetch on json file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from 'react';
 import Banner from '../../components/Banner';
 import bannerHome from '../../assets/bannerHome.png';
 import Gallery from '../../Gallery';
-import { housingsData } from '../../data/housingsData.json';
-console.log({ housingsData });
+// import housingsData from '../../data/housingsData.json';
+// console.log(housingsData);
 
 /**
  * Création de la page d'accueil
@@ -16,21 +16,48 @@ console.log({ housingsData });
 // initialisation des données json
 
 const Home = () => {
-  const [Data, setData] = useState([]);
+  // const [Data, setData] = useState([]);
+
+  const [accomodationData, setData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(`../../data/housingsData.json`);
-        const housingsData = await response.json();
-        console.log(housingsData);
-        setData(housingsData);
+        const response = await fetch(`./housingsData.json`);
+        const accomodationData = await response.json();
+        setData(accomodationData);
+        console.log(accomodationData);
       } catch (err) {
         console.log(err);
       }
     }
     fetchData();
   }, []);
+
+  // const url = './housingsData.json';
+  // fetch(url)
+  //   .then((response) => {
+  //     console.log(response);
+  //   })
+  //   .then((results) => {
+  //     console.log(results);
+  //     console.log(results.body);
+  //   });
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     fetch
+  //     try {
+  //       const response = await fetch(`./housingsData.json`);
+  //       const housingsData = await response.json();
+  //       console.log(housingsData);
+  //       setData(housingsData);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
 
   return (
     <main>
