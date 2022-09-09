6901d10f802feee429507eccd0a281f17411c964
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-09-09 17:09:11 +0200

    display housingsData titles on homepage w/ map function on json fetch w/ useState & useEffect

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,8 +3,6 @@ import { useState, useEffect } from 'react';
 import Banner from '../../components/Banner';
 import bannerHome from '../../assets/bannerHome.png';
 import Gallery from '../../Gallery';
-// import housingsData from '../../data/housingsData.json';
-// console.log(housingsData);
 
 /**
  * Création de la page d'accueil
@@ -16,18 +14,18 @@ import Gallery from '../../Gallery';
 // initialisation des données json
 
 const Home = () => {
-  // const [Data, setData] = useState([]);
-
   const [accomodationData, setData] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch(
-          `https://raw.githubusercontent.com/8UK0W5K1/OCR_P11_KAZA/main/public/data/housings.json?token=GHSAT0AAAAAABWUPTTQUMB56GANJ5IV6BEOYY3HCEA`
+          `https://raw.githubusercontent.com/8UK0W5K1/OCR_P11_KAZA/main/src/data/housingsData.json`
         );
         const accomodationData = await response.json();
         setData(accomodationData);
+        setIsLoading(false);
         console.log(accomodationData);
       } catch (err) {
         console.log(err);
@@ -36,34 +34,12 @@ const Home = () => {
     fetchData();
   }, []);
 
-  // const url = './housingsData.json';
-  // fetch(url)
-  //   .then((response) => {
-  //     console.log(response);
-  //   })
-  //   .then((results) => {
-  //     console.log(results);
-  //     console.log(results.body);
-  //   });
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     fetch
-  //     try {
-  //       const response = await fetch(`./housingsData.json`);
-  //       const housingsData = await response.json();
-  //       console.log(housingsData);
-  //       setData(housingsData);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   }
-  //   fetchData();
-  // }, []);
-
   return (
     <main>
       <Banner image={bannerHome} type='Home' />
+      {isloading
+        ? 'Loading...'
+        : accomodationData.map((data) => <li key={data.id}>{data.title}</li>)}
       <Gallery />
     </main>
   );
